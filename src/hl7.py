3b32a04799f480c121470db4cd490d642bf77b33
Sudhi S <sudhis@live.com>
2023-12-28 14:47:54 +0530

    REDME  commit

--- src/hl7.py
+++ src/hl7.py
@@ -44,13 +44,13 @@ class HL7Utils:
               subsegment_values = field.split('^')
               if len(subsegment_values) >1:
                 for j, subfieldValue in enumerate(subsegment_values):
-                  field_name = segment_type + '.'+ str(index) +  '.' + str(j)
+                  field_name = segment_type + '.'+ str(index) +  '.' + str(j+1)
                   if subfieldValue != ' ' and subfieldValue != '':
                     json_segment[field_name.strip()] = subfieldValue
                     sub_subsegment_values = subfieldValue.split('~')
                     if len(sub_subsegment_values) >1:
                       for k, second_subfieldValue in enumerate(sub_subsegment_values):
-                        field_name = segment_type + '.'+ str(index) +  '.' + str(j)+  '.' + str(k+1)
+                        field_name = segment_type + '.'+ str(index) +  '.' + str(j+1)+  '.' + str(k+1)
                         if second_subfieldValue != ' ' and second_subfieldValue != '':
                           json_segment[field_name.strip()] = second_subfieldValue
 
@@ -98,6 +98,14 @@ class HL7Utils:
         "PID.3": "Patient ID (Internal ID)",
         "PID.4": "Alternate Patient ID",
         "PID.5": "Patient Name",
+        "PID.5.1":"Family Name",
+        "PID.5.2":"Given Name",
+        "PID.5.3":"Middle Initial Or Name",
+        "PID.5.4":"Suffix",
+        "PID.5.5":"Prefix",
+        "PID.5.6":"Degree",
+        "PID.5.7":"Name Type Code",
+        "PID.5.8":"Name Representation Code",
         "PID.6": "Mother's Maiden Name",
         "PID.7": "Date of Birth",
         "PID.8": "Sex",
@@ -119,6 +127,12 @@ class HL7Utils:
         "PID.24": "Multiple Birth Indicator",
         "PID.25": "Birth Order",
         "PID.26": "Citizenship",
+        "PID.26.1": "Citizenship Identifier",
+        "PID.26.2": "Citizenship Text",
+        "PID.26.3": "Citizenship Name Of Coding System",
+        "PID.26.4": "Citizenship Alternate Components",
+         "PID.26.5": "Citizenship Alternate Text",
+         "PID.26.6": "Citizenship  Name Of Alternate Coding System",
         "PID.27": "Veterans Military Status",
         "PID.28": "Nationality Code",
         "PID.29": "Patient Death Date and Time",
